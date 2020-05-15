077eeb225a0ec71ef9f4357894e238ad3f5f9590
Praneeth <praneethbedapudi@gmail.com>
2020-05-15 18:33:08 +0530

    update benchmark scripts

--- benchmark_utils/deepsegment_en/benchmark_native.py
+++ benchmark_utils/deepsegment_en/benchmark_native.py
@@ -14,11 +14,11 @@ for _ in range(3):
 
 # Expected result is [['I was hungry', 'i ordered a pizza and i went to the movies', 'which movie did you go to', 'i watched dark knight rises', 'oh how was it', 'it was a good movie', 'yeah thought so']]
 
-in_data = list(example * 10000)
+in_data = list(example * 8192)
 
-for batch_size in [1, 32, 128, 512, 1024]:
+for batch_size in [1, 32, 128]:
 
-    dump(in_data[:batch_size], open(f"{batch_size}.json", "w"))
+    dump({'data': in_data[:batch_size]}, open(f"{batch_size}.json", "w"))
 
     start = time()
     results = model.segment(in_data, batch_size)

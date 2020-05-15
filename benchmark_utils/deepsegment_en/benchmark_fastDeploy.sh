9a718addbf32b378b577b8e2043b915d26a633cd
Praneeth <praneethbedapudi@gmail.com>
2020-05-15 18:47:35 +0530

    update benchmark scripts

--- benchmark_utils/deepsegment_en/benchmark_fastDeploy.sh
+++ benchmark_utils/deepsegment_en/benchmark_fastDeploy.sh
@@ -1,3 +1,5 @@
+autocannon -c 1 -t 1000 -a 8192  -m POST -i 1.json -H 'Content-Type: application/json' http://localhost:6788/sync
+
 autocannon -c 16 -t 1000 -a 8192  -m POST -i 1.json -H 'Content-Type: application/json' http://localhost:6788/sync
 
 autocannon -c 256 -t 1000 -a 8192  -m POST -i 1.json -H 'Content-Type: application/json' http://localhost:6788/sync
@@ -7,12 +9,17 @@ autocannon -c 2048 -t 1000 -a 8192  -m POST -i 1.json -H 'Content-Type: applicat
 
 
 
+autocannon -c 1 -t 1000 -a 256  -m POST -i 32.json -H 'Content-Type: application/json' http://localhost:6788/sync
+
 autocannon -c 16 -t 1000 -a 256  -m POST -i 32.json -H 'Content-Type: application/json' http://localhost:6788/sync
 
 autocannon -c 64 -t 1000 -a 256  -m POST -i 32.json -H 'Content-Type: application/json' http://localhost:6788/sync
 
 autocannon -c 256 -t 1000 -a 256  -m POST -i 32.json -H 'Content-Type: application/json' http://localhost:6788/sync
 
+
+
+autocannon -c 1 -t 1000 -a 64  -m POST -i 128.json -H 'Content-Type: application/json' http://localhost:6788/sync
 
 autocannon -c 16 -t 1000 -a 64  -m POST -i 128.json -H 'Content-Type: application/json' http://localhost:6788/sync
 

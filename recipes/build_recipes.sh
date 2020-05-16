316cba7e67b85817c7a89d5e26ae7f84b2cef599
Praneeth <praneethbedapudi@gmail.com>
2020-05-16 18:45:06 +0530

    update build_recipes.sh
    Signed-off-by: Praneeth <praneethbedapudi@gmail.com>

--- recipes/build_recipes.sh
+++ recipes/build_recipes.sh
@@ -1,3 +1,4 @@
+#============================== DeepSegment =============================
 docker rmi notaitech/fastdeploy-recipe:deepsegment_en
 docker rm temp
 python3 ../cli/fastDeploy.py --build temp --source_dir deepsegment/ --verbose --base tf_1_14_cpu --extra_config '{"LANG": "en"}' --port 127.0.0.1:6788
@@ -18,6 +19,7 @@ python3 ../cli/fastDeploy.py --build temp --source_dir deepsegment/ --verbose --
 docker commit temp notaitech/fastdeploy-recipe:deepsegment_fr
 docker push notaitech/fastdeploy-recipe:deepsegment_fr
 
+# ======================= NudeCLassifier ===============================
 
 docker rmi notaitech/fastdeploy-recipe:nudeclassifier
 docker rm temp
@@ -25,6 +27,7 @@ python3 ../cli/fastDeploy.py --build temp --source_dir nudeclassifier/ --verbose
 docker commit temp notaitech/fastdeploy-recipe:nudeclassifier
 docker push notaitech/fastdeploy-recipe:nudeclassifier
 
+# ======================= EfficientNet ===============================
 
 docker rmi notaitech/fastdeploy-recipe:efficientnet_b0
 docker rm temp
@@ -52,6 +55,9 @@ python3 ../cli/fastDeploy.py --build temp --source_dir efficientnet_imagenet/ --
 docker commit temp notaitech/fastdeploy-recipe:efficientnet_b7
 docker push notaitech/fastdeploy-recipe:efficientnet_b7
 
+
+# =========================== CRAFT ========================
+
 docker rmi notaitech/fastdeploy-recipe:craft_text_detection
 docker rm temp
 python3 ../cli/fastDeploy.py --build temp --source_dir craft_text_detection/ --verbose --base tf_1_14_cpu --extra_config '{}' --port 127.0.0.1:6788
@@ -59,6 +65,7 @@ docker commit temp notaitech/fastdeploy-recipe:craft_text_detection
 docker push notaitech/fastdeploy-recipe:craft_text_detection
 
 
+# ========================== Transformers ========================
 
 docker rmi notaitech/fastdeploy-recipe:transformer_ner
 docker rm temp
@@ -87,8 +94,26 @@ python3 ../cli/fastDeploy.py --build temp --source_dir huggingface_transformers/
 docker commit temp notaitech/fastdeploy-recipe:transformer_translation_en_to_fr
 docker push notaitech/fastdeploy-recipe:transformer_translation_en_to_fr
 
+
+# ======================== YAMNet ==============================
+
 docker rmi notaitech/fastdeploy-recipe:audio_classification_yamnet
 docker rm temp
 python3 ../cli/fastDeploy.py --build temp --source_dir audio_classification_yamnet/ --verbose --base tf_2_1_cpu --extra_config '{}' --port 127.0.0.1:6788
 docker commit temp notaitech/fastdeploy-recipe:audio_classification_yamnet
 docker push notaitech/fastdeploy-recipe:audio_classification_yamnet
+
+# ====================== VOSK-API =====================================
+
+docker rmi notaitech/fastdeploy-recipe:kaldi_vosk-en_us-small
+docker rm temp
+python3 ../cli/fastDeploy.py --build temp --source_dir kaldi_asr_vosk/ --verbose --base base-v0.1 --extra_config '{"MODEL_ZIP_URL": "http://alphacephei.com/kaldi/models/vosk-model-small-en-us-0.3.zip"}' --port 127.0.0.1:6788
+docker commit temp notaitech/fastdeploy-recipe:kaldi_vosk-en_us-small
+docker push notaitech/fastdeploy-recipe:kaldi_vosk-en_us-small
+
+docker rmi notaitech/fastdeploy-recipe:kaldi_vosk-en_us-aspire
+docker rm temp
+python3 ../cli/fastDeploy.py --build temp --source_dir kaldi_asr_vosk/ --verbose --base base-v0.1 --extra_config '{"MODEL_ZIP_URL": "http://alphacephei.com/kaldi/models/vosk-model-en-us-aspire-0.2.zip"}' --port 127.0.0.1:6788
+docker commit temp notaitech/fastdeploy-recipe:kaldi_vosk-en_us-aspire
+docker push notaitech/fastdeploy-recipe:kaldi_vosk-en_us-aspire
+

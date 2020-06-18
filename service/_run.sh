5cf59d83262cacc9d7b429e1d10c73fe9cc14725
Praneeth <praneethbedapudi@gmail.com>
2020-06-18 18:26:44 +0530

    option to auto-update fastDeploy source
    Signed-off-by: Praneeth <praneethbedapudi@gmail.com>

--- service/_run.sh
+++ service/_run.sh
@@ -1,3 +1,28 @@
+if [ "$UPDATE" = "true" ]; then
+    python3 -m pip install pydload
+
+    echo 'Updating _app.py'
+    rm _app.py
+    python3 -m pydload https://raw.githubusercontent.com/notAI-tech/fastDeploy/master/service/_app.py _app.py
+
+    echo 'Updating _generate_run_sh.py'
+    rm _generate_run_sh.py
+    python3 -m pydload https://raw.githubusercontent.com/notAI-tech/fastDeploy/master/service/_generate_run_sh.py _generate_run_sh.py
+
+    echo 'Updating _loop.py'
+    rm _loop.py
+    python3 -m pydload https://raw.githubusercontent.com/notAI-tech/fastDeploy/master/service/_loop.py _loop.py
+
+    echo 'Updating _manager_loop.py'
+    rm _manager_loop.py
+    python3 -m pydload https://raw.githubusercontent.com/notAI-tech/fastDeploy/master/service/_manager_loop.py _manager_loop.py
+
+    echo 'Updating _utils.py'
+    rm _utils.py
+    python3 -m pydload https://raw.githubusercontent.com/notAI-tech/fastDeploy/master/service/_utils.py _utils.py
+
+fi
+
 if [ ! -f "_success" ]; then
 	python3 -u _setup.py
 fi

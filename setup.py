88d374524bd435bf224036209787ee84e8a8f03b
Bedapudi Praneeth <praneethbedapudi@gmail.com>
2021-09-27 00:20:36 +0530

    bump version
    Signed-off-by: Bedapudi Praneeth <praneethbedapudi@gmail.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/notAI-tech/fastDeploy"
 EMAIL = "praneeth@bpraneeth.com"
 AUTHOR = "BEDAPUDI PRANEETH"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "1.0-rc3"
+VERSION = "1.0-rc4"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["requests", "gevent", "falcon", "diskcache", "ujson"]
@@ -103,7 +103,7 @@ setup(
     packages=find_packages(exclude=("tests",)),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
-    entry_points={"console_scripts": ["fd_loop=fastdeploy:loop"],},
+    entry_points={"console_scripts": ["fastdeploy=fastdeploy"]},
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,

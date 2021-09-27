bf4d7deb5aebfaf35c1fbea735b9ec89b0e6fc0a
Bedapudi Praneeth <praneethbedapudi@gmail.com>
2021-09-27 10:33:55 +0530

    bump version
    Signed-off-by: Bedapudi Praneeth <praneethbedapudi@gmail.com>

--- fastdeploy/__main__.py
+++ fastdeploy/__main__.py
@@ -1,40 +1,57 @@
 import os
 import sys
-import argparse
 import subprocess
 
-parser = argparse.ArgumentParser(description="CLI for fastDeploy")
-parser.add_argument(
-    "--recipe", type=str, help="Path to your recipe folder", required=True
-)
+RECIPE = os.getenv("RECIPE")
+MODE = os.getenv("MODE")
+QUEUE_DIR = os.getenv("QUEUE_DIR")
 
-parser.add_argument(
-    "--mode",
-    type=str,
-    help='One of, ["loop", "rest", "websocket", "build_rest"]',
-    required=True,
-)
+if RECIPE is None or MODE is None:
+    import argparse
 
-parser.add_argument(
-    "--queue_dir", type=str, help='Defaults to "--recipe" folder', required=False
-)
+    parser = argparse.ArgumentParser(description="CLI for fastDeploy")
+    parser.add_argument(
+        "--recipe",
+        type=str,
+        help="Path to your recipe folder; env: RECIPE",
+        required=True,
+    )
 
-parser.add_argument(
-    "--base",
-    type=str,
-    help='docker image to use as base. defaults to "auto"',
-    required=False,
-)
+    parser.add_argument(
+        "--mode",
+        type=str,
+        help='One of, ["loop", "rest", "websocket", "build_rest"]; env: MODE',
+        required=True,
+    )
 
-args = parser.parse_args()
-sys.path.append(args.recipe)
+    parser.add_argument(
+        "--queue_dir",
+        type=str,
+        help='Defaults to "--recipe" folder; end: QUEUE_DIR',
+        required=False,
+    )
 
-QUEUE_DIR = args.recipe
+    parser.add_argument(
+        "--base",
+        type=str,
+        help='docker image to use as base. defaults to "auto"',
+        required=False,
+    )
+
+    args = parser.parse_args()
+    sys.path.append(RECIPE)
+
+    QUEUE_DIR = args.recipe
+    if args.queue_dir:
+        QUEUE_DIR = args.queue_dir
+
+    MODE = args.mode
+    RECIPE = args.recipe
+
+    if args.queue_dir:
+        queue_dir = args.QUEUE_DIR
 
 QUEUE_NAME = os.getenv(f"QUEUE_NAME", f"default")
-
-if args.queue_dir:
-    queue_dir = args.QUEUE_DIR
 
 
 def loop():
@@ -59,16 +76,16 @@ def rest():
 def build_rest():
     dockerfile_lines = []
 
-    if not args.base:
+    if not BASE:
         base = "python:3.6-slim"
     else:
-        base = args.base
+        base = BASE
 
     dockerfile_lines.append(f"FROM {base}")
     dockerfile_lines.append(
         f"RUN python3 -m pip install --upgrade --no-cache-dir pip fastdeploy gunicorn"
     )
-    if os.path.exists(os.path.join(args.recipe, "extras.sh")):
+    if os.path.exists(os.path.join(RECIPE, "extras.sh")):
         dockerfile_lines.append(f"COPY extras.sh /extras.sh")
         dockerfile_lines.append(f"RUN bash /extras.sh")
 
@@ -77,7 +94,6 @@ def build_rest():
         f"RUN python3 -m pip install --no-cache-dir -r /requirements.txt"
     )
 
-    # recipe_base_name = f'/{os.path.basename(args.recipe).strip("./")}'
     # if not recipe_base_name:
     recipe_base_name = "/recipe"
 
@@ -86,31 +102,31 @@ def build_rest():
     dockerfile_lines.append(f"RUN cd {recipe_base_name} && python3 predictor.py")
 
     dockerfile_lines.append(
-        f'CMD python3 -m fastdeploy --recipe /recipe --mode loop ; python3 -m fastdeploy --recipe /recipe --mode {args.mode.split("build_")[1]} \n'
+        f'CMD python3 -m fastdeploy --recipe /recipe --mode loop ; python3 -m fastdeploy --recipe /recipe --mode {MODE.split("build_")[1]} \n'
     )
 
-    dockerfile_path = os.path.join(args.recipe, "fastDeploy.auto_dockerfile")
+    dockerfile_path = os.path.join(RECIPE, "fastDeploy.auto_dockerfile")
 
     _f = open(dockerfile_path, "w")
     _f.write("\n".join(dockerfile_lines))
     _f.flush()
     _f.close()
 
-    docker_image_name = f"fastdeploy_{os.path.basename(os.path.abspath(args.recipe))}".strip(
+    docker_image_name = f"fastdeploy_{os.path.basename(os.path.abspath(RECIPE))}".strip(
         "/"
     ).lower()
 
     subprocess.call(
-        f"docker build -f {dockerfile_path} -t {docker_image_name} {args.recipe}",
+        f"docker build -f {dockerfile_path} -t {docker_image_name} {RECIPE}",
         shell=True,
     )
 
 
-if args.mode == "loop":
+if MODE == "loop":
     loop()
 
-if args.mode == "rest":
+if MODE == "rest":
     rest()
 
-if args.mode == "build_rest":
+if MODE == "build_rest":
     build_rest()

1453e4bc3684f856794c5922ca4eb43d198e6524
Bedapudi Praneeth <praneethbedapudi@gmail.com>
2022-04-05 13:00:04 +0530

    refactor
    Signed-off-by: Bedapudi Praneeth <praneethbedapudi@gmail.com>

--- fastdeploy-ui/src/App.svelte
+++ fastdeploy-ui/src/App.svelte
@@ -1,19 +1,52 @@
-
 <svelte:head><link rel="stylesheet" href="https://unpkg.com/carbon-components-svelte@0.30.0/css/g10.css" /></svelte:head>
 
 <script>
-	import {CopyButton, Theme, ButtonSet, RadioButtonGroup, RadioButton, Checkbox, Button, TextArea, Grid, Column, Row, FileUploader, CodeSnippet } from 'carbon-components-svelte';
-	let theme = "g90";
+import {
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    SideNav,
+    SideNavItems,
+    SideNavMenu,
+    SideNavMenuItem,
+    SideNavLink,
+    SideNavDivider,
+    SkipToContent,
+    Content,
+    Grid,
+    Row,
+    Column,
 
-	import { onMount } from "svelte";
+    CopyButton,
+	ExpandableTile,
+    Theme,
+    Tile,
+    ButtonSet,
+    Checkbox,
+    Button,
+    TextArea,
+    FileUploader,
+    CodeSnippet
+} from 'carbon-components-svelte';
 
-	
-	let multi_input = 'Loading ...';
-	let async_check_box_bool = false;
-	
-	let result = 'Post to see result';
-	let curl_text = 'Loading ...'
-	let JSON_CURL_TEXT = `# Sync request to fastdeploy
+import ChartComboStacked32 from "carbon-icons-svelte/lib/ChartComboStacked32";
+import Catalog32 from "carbon-icons-svelte/lib/Catalog32";
+import Home32 from "carbon-icons-svelte/lib/Home32";
+
+let theme = "g90";
+let isSideNavOpen = false;
+
+import {
+    onMount
+} from "svelte";
+
+let multi_input = 'Loading ...';
+let async_check_box_bool = false;
+
+let result = 'Post to see result';
+let curl_text = 'Loading ...'
+let JSON_CURL_TEXT = `# Sync request to fastdeploy
 curl -d 'EXAMPLE' -H "Content-Type: application/json" -X POST "http://localhost:8080/infer"
 
 # Async request to fastdeploy
@@ -21,77 +54,123 @@ curl -d 'EXAMPLE' -H "Content-Type: application/json" -X POST "http://localhost:
 
 # Result for async request
 curl -d '{"unique_id": "REQUEST_ID"}' -H "Content-Type: application/json" -X POST "http://localhost:8080/res"`
-	let META = {};
+let META = {};
 
-	
+onMount(async () => {
+    fetch("/meta")
+        .then(response => response.json())
+        .then(data => {
+            META = data
+            multi_input = JSON.stringify(data["example"], null, 4);
 
-	onMount(async () => {
-		fetch("/meta")
-		.then(response => response.json())
-		.then(data => {
-				META = data
-				multi_input = JSON.stringify(data["example"], null, 4);
+            if (!META['is_file_input']) {
+                curl_text = JSON_CURL_TEXT.replaceAll('EXAMPLE', JSON.stringify(data["example"]))
+            }
+            console.log("META", META);
+        }).catch(error => {
+            console.log(error);
+            return [];
+        });
+});
 
-				if (!META['is_file_input']) {
-					curl_text = JSON_CURL_TEXT.replaceAll('EXAMPLE', JSON.stringify(data["example"]))
-				}
-				console.log("META", META);
-		}).catch(error => {
-			console.log(error);
-			return [];
-		});
-		});
-		
-	async function getResult () {
-		console.log(multi_input)
-		curl_text = JSON_CURL_TEXT.replaceAll('EXAMPLE', JSON.stringify(JSON.parse(multi_input)))
-		const res = await fetch('/infer', {
-			method: 'POST',
-			headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-					},
-			body: JSON.stringify({"data": JSON.parse(multi_input)})
-		})
-		
-		const json = await res.json()
-		result = JSON.stringify(json['prediction'], null, 4)
+async function getResult() {
+    console.log(multi_input)
+    curl_text = JSON_CURL_TEXT.replaceAll('EXAMPLE', JSON.stringify(JSON.parse(multi_input)))
+    const res = await fetch('/infer', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "data": JSON.parse(multi_input)
+        })
+    })
 
-	}
+    const json = await res.json()
+    result = JSON.stringify(json['prediction'], null, 4)
+
+}
 </script>
 
-<Theme bind:theme />
+<Header company="fastDeploy" platformName="monitor" bind:isSideNavOpen>
+	<svelte:fragment slot="skip-to-content">
+	  <SkipToContent />
+	</svelte:fragment>
+	
+	<HeaderNav>
+	  <HeaderNavItem href="/" text="" />
+	  <HeaderNavItem href="/" text="" />
+	  <HeaderNavItem href="/" text="" />
+	</HeaderNav>
 
-<!-- <RadioButtonGroup legendText="Carbon theme" bind:selected={theme}>
-  {#each ["white", "g10", "g80", "g90", "g100"] as value}
-    <RadioButton labelText={value} {value} />
-  {/each}
-</RadioButtonGroup> -->
+	<SideNav bind:isOpen={isSideNavOpen} rail>
+		<SideNavItems>
+			<SideNavLink icon={Home32} text="Home" href="/" isSelected />
+			<SideNavLink icon={Catalog32} text="Logs (coming soon)" href="/" />
+			<SideNavLink icon={ChartComboStacked32} text="Graphs (coming soon)" href="/" />
+		</SideNavItems>
+	</SideNav>
+</Header>
 
-<Grid padding=true>
-	<Row padding=true>
-		<Column>
-			<Row>
-				<TextArea bind:value={multi_input} placeholder={multi_input}/>
-			</Row>
+<Content>
+	<Grid padding=true>
+		<Row padding=true>
+			<Column>
+				<Row>
+					<TextArea bind:value={multi_input} placeholder={multi_input}/>
+				</Row>
 
-			<Row></Row>
+				<FileUploader multiple buttonLabel="Add files" status="complete" disabled=true/>
 
-			<FileUploader multiple buttonLabel="Add files" status="complete" disabled=true/>
+				<ButtonSet stacked=true>
+					<Checkbox labelText="Async request" bind:async_check_box_bool/>
+					<Button kind="primary" outline on:click={getResult}>Post</Button>
+				</ButtonSet>
+			</Column>
 
-			<ButtonSet stacked=true>
-				<Checkbox labelText="Async request" bind:async_check_box_bool/>
-				<Button kind="primary" outline on:click={getResult}>Post</Button>
-			</ButtonSet>
+			<Column>
+				<CopyButton bind:text={result} feedback="Copied to clipboard" />
+				<Tile light> {result} </Tile>
+				<CodeSnippet wrapText type="multi" code={curl_text} />
+			</Column>
+		</Row>
 
+		<Row padding=true>
+			<Column>
+				<ExpandableTile>
+					<div slot="above" style="height: 5rem">Predictor stats</div>
+					<div slot="below" style="height: 5rem">Below the fold content here</div>
+				</ExpandableTile>
+			</Column>
 
+			<Column>
+				<ExpandableTile>
+					<div slot="above" style="height: 5rem">API stats</div>
+					<div slot="below" style="height: 5rem">Below the fold content here</div>
+				</ExpandableTile>
+			</Column>
 
-		</Column>
+			<Column>
+				<ExpandableTile>
+					<div slot="above" style="height: 5rem">Wait times</div>
+					<div slot="below" style="height: 5rem">Below the fold content here</div>
+				</ExpandableTile>
+			</Column>
 
-		<Column>
-			<CopyButton bind:text={result} feedback="Copied to clipboard" />
-			<TextArea light readonly bind:value={result} placeholder={result}/>
-			<CodeSnippet wrapText type="multi" code={curl_text} />
-		</Column>
-	</Row>
-</Grid>
+			<Column>
+				<ExpandableTile>
+					<div slot="above" style="height: 5rem">Batch size stats</div>
+					<div slot="below" style="height: 5rem">Below the fold content here</div>
+				</ExpandableTile>
+			</Column>
+
+			<Column>
+				<ExpandableTile>
+					<div slot="above" style="height: 5rem">Above the fold content here</div>
+					<div slot="below" style="height: 5rem">Below the fold content here</div>
+				</ExpandableTile>
+			</Column>
+		</Row>
+	</Grid>
+</Content>

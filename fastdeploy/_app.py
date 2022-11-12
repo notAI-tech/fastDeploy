ce13f8d116789ca2050b54de2ae957b3b567cd26
Shashi Kant <shashikg.iitk@gmail.com>
2022-11-12 13:47:25 +0530

    return not ready in case prediction loop is not started

--- fastdeploy/_app.py
+++ fastdeploy/_app.py
@@ -329,30 +329,39 @@ class Health(object):
 
 class Readiness(object):
     def on_get(self, req, resp):
-        total_response_time = _utils.META_INDEX["time_per_example"]
-        total_requests = 1
-        for i in range(min(100, len(_utils.METRICS_CACHE))):
-            _, _metrics, _ = _utils.METRICS_CACHE[i]
-            total_response_time += (_metrics["responded"]-_metrics["received"])
-            total_requests += _metrics["predicted_in_batch"]
-
-        total_reqs_in_queue = 0
-        for key, value in _utils.REQUEST_INDEX.items():
-            total_reqs_in_queue += len(value[0])
-
-        approx_wait_time = total_reqs_in_queue*(total_response_time/total_requests)
-        max_wait_time = float(req.params.get("waittime"))
-
-        if (max_wait_time) and (approx_wait_time > max_wait_time):
-                resp.status = falcon.HTTP_503
-                resp.media = {
-                    "wait_time": f"current wait time is {approx_wait_time}. not ready"
-                }
-        else:
-            resp.status = falcon.HTTP_200
+        
+        if _utils.META_INDEX["last_prediction_loop_start_time"] == 0:
+            # If prediction loop not started ==> not ready
+            resp.status = falcon.HTTP_503
             resp.media = {
-                "wait_time": f"current wait time is {approx_wait_time}"
+                "ready_status": f"Not ready, prediction loop not started"
             }
+        else:
+            # If prediction loop started, check for wait time
+            total_response_time = _utils.META_INDEX["time_per_example"]
+            total_requests = 1
+            for i in range(min(100, len(_utils.METRICS_CACHE))):
+                _, _metrics, _ = _utils.METRICS_CACHE[i]
+                total_response_time += (_metrics["responded"]-_metrics["received"])
+                total_requests += _metrics["predicted_in_batch"]
+
+            total_reqs_in_queue = 0
+            for key, value in _utils.REQUEST_INDEX.items():
+                total_reqs_in_queue += len(value[0])
+
+            approx_wait_time = total_reqs_in_queue*(total_response_time/total_requests)
+            max_wait_time = float(req.params.get("waittime"))
+
+            if (max_wait_time) and (approx_wait_time > max_wait_time):
+                    resp.status = falcon.HTTP_503
+                    resp.media = {
+                        "ready_status": f"Not ready, current wait time is {approx_wait_time}s which is more than max wait time of {max_wait_time}s"
+                    }
+            else:
+                resp.status = falcon.HTTP_200
+                resp.media = {
+                    "ready_status": f"Ready, current wait time is {approx_wait_time}s"
+                }
 
 class Meta(object):
     def on_get(self, req, resp):
